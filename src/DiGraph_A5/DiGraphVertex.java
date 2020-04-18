23f5faf9edb27c13c9fed39b666060d1d546d92e
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2020-04-18 16:59:30 -0400

    Vertex class made. Need to make edge class.

---
+++ src/DiGraph_A5/DiGraphVertex.java
@@ -0,0 +1,32 @@
+package DiGraph_A5;
+
+import java.util.*;
+
+
+public class DiGraphVertex {
+	private long idNum;
+	private String label;
+	private LinkedList<DiGraphEdge> out_edge = new LinkedList<DiGraphEdge>();
+	private LinkedList<DiGraphEdge> in_edge = new LinkedList<DiGraphEdge>();
+	
+	public DiGraphVertex(long idNum, String label) {
+	    this.idNum = idNum;
+	    this.label = label;
+	  }
+	
+	public long getIdNum() { return idNum;}
+	public String getLabel() { return label;}
+	public void addOutEdge(DiGraphEdge out) {
+		out_edge.add(out);
+	}
+	public void addInEdge(DiGraphEdge in) {
+		out_edge.add(in);
+	}
+	public void removeOutEdge(DiGraphEdge out) {
+		out_edge.remove(out);
+	}
+	public void removeInEdge(DiGraphEdge in) {
+		out_edge.remove(in);
+	}
+	
+}
