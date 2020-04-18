77a45edd5b89efe6bbd8466d572aa5ef6c7df939
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2020-04-18 18:06:50 -0400

    finished add vertex

--- src/DiGraph_A5/DiGraph.java
+++ src/DiGraph_A5/DiGraph.java
@@ -1,24 +1,43 @@
 package DiGraph_A5;
 
-public class DiGraph implements DiGraphInterface {
+import java.util.*;
 
+public class DiGraph implements DiGraphInterface {
+	LinkedList<Long> graphVert_idNums;
+	LinkedList<Long> graphEdge_idNums;
+	HashMap<String, DiGraphVertex> graph_vertexs;  
 	// in here go all your data and methods for the graph
 
-	public DiGraph() { // default constructor
-		// explicitly include this
-		// we need to have the default constructor
-		// if you then write others, this one will still be there
+	public DiGraph() {
+		graphVert_idNums = new LinkedList<Long>();
+		graphEdge_idNums = new LinkedList<Long>();
+		graph_vertexs = new HashMap<String, DiGraphVertex>();
 	}
 
 	@Override
 	public boolean addNode(long idNum, String label) {
-		// TODO Auto-generated method stub
-		return false;
+		if(graphVert_idNums.contains(idNum) || idNum < 0) {
+			return false;
+		}
+		if(graph_vertexs.containsKey(label)) {
+			return false;
+		}
+		DiGraphVertex vert = new DiGraphVertex(idNum, label);
+		graphVert_idNums.add(idNum);
+		graph_vertexs.put(label, vert);
+		return true;
 	}
 
 	@Override
 	public boolean addEdge(long idNum, String sLabel, String dLabel, long weight, String eLabel) {
-		// TODO Auto-generated method stub
+		if(graphEdge_idNums.contains(idNum) || idNum < 0) {
+			return false;
+		}
+		if(!(graph_vertexs.containsKey(sLabel)) || !(graph_vertexs.containsKey(dLabel))) {
+			return false;
+		}
+		// TODO check if there is already edge between slabel and dlabel
+		//if we make it this far: add outEdge and inEdge
 		return false;
 	}
 
@@ -36,8 +55,7 @@ public class DiGraph implements DiGraphInterface {
 
 	@Override
 	public long numNodes() {
-		// TODO Auto-generated method stub
-		return 0;
+		return graph_vertexs.size();
 	}
 
 	@Override
